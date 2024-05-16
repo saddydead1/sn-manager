fe54cb697155f5759acac539ddf9b33ad438a87e
saddydead <luckybeyhuk@gmail.com>
2024-05-16 18:06:43 +0300

    🍀🍀🍀

--- client/Package.swift
+++ client/Package.swift
@@ -4,17 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "socket.io-test",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         .executable(name: "socket.io-test", targets: ["YourTargetName"])
     ],
     dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.1.0"))
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0")
     ],
     targets: [
         .executableTarget(
             name: "YourTargetName",
             dependencies: [
-                .product(name: "SocketIO", package: "socket.io-client-swift"),
+                .product(name: "AsyncHTTPClient", package: "async-http-client"),
             ],
             path: "./Sources/"),
     ]
--- client/Sources/main.swift
+++ client/Sources/main.swift
@@ -1,6 +1,15 @@
 import Foundation
+import AsyncHTTPClient
 
 
-SocketHandler.sharedInstance.Connect()
+let addr = "http://127.0.0.1:7072"
 
+var request = HTTPClientRequest(url: addr)
+let response = try await HTTPClient.shared.execute(request, timeout: .seconds(30))
+print("HTTP head", response)
+if response.status == .ok {
+    let body = try await response.body.collect(upTo: 1024 * 1024) // 1 MB
 
+} else {
+    // handle remote error
+}
