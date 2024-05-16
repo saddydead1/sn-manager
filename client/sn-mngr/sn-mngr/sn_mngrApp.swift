022088e76ba2f36d55fb7b710b93ce506ec1de9e
saddydead <luckybeyhuk@gmail.com>
2024-05-16 23:01:49 +0300

    переделал клиентскую часть под проект xcode

---
+++ client/sn-mngr/sn-mngr/sn_mngrApp.swift
@@ -0,0 +1,17 @@
+//
+//  sn_mngrApp.swift
+//  sn-mngr
+//
+//  Created by saddydead on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct sn_mngrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
