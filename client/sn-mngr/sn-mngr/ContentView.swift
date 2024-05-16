9cd13d0aab38ba36b07880fce32a7b3e35c71939
saddydead <luckybeyhuk@gmail.com>
2024-05-16 23:13:32 +0300

    simple gui

--- client/sn-mngr/sn-mngr/ContentView.swift
+++ client/sn-mngr/sn-mngr/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Sonoma Manager v3").font(.title2)
+            Button("test") {
+                print(123)
+            }
         }
         .padding()
     }
