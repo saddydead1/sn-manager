923387e4d8d10985b0f354b79423966391d3e404
saddydead <luckybeyhuk@gmail.com>
2024-05-18 13:22:37 +0300

    simple login screen

--- client/sn-mngr/sn-mngr/ContentView.swift
+++ client/sn-mngr/sn-mngr/ContentView.swift
@@ -7,18 +7,57 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @Bindable var viewMod: LoginViewModel
+    
     var body: some View {
         VStack {
-            Text("Sonoma Manager v3").font(.title2)
-            Button("test") {
-                print(123)
+            if !viewMod.auth {
+                Text("Sonoma Manager v3").font(.title2)
+                
+                VStack(spacing: 15){
+                    TextField("Логин", text: $viewMod.user.name)
+                        .customStyle()
+                    SecureField("Пароль", text: $viewMod.user.pass)
+                        .customStyle()
+                }
+            
+                Button("Войти") {
+                    viewMod.login()
+                }
+            } else {
+                Text("123")
+                
+                Button("Выйти") {
+                    viewMod.logout()
+                }
             }
+        
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(viewMod: LoginViewModel())
+}
+
+
+struct StyleView: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(width: 300, height: 40)
+            .background(.gray.opacity(0.03))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    
+}
+
+extension View {
+    func customStyle() -> some View {
+        modifier(StyleView())
+    }
 }
--- client/sn-mngr/sn-mngr/sn_mngrApp.swift
+++ client/sn-mngr/sn-mngr/sn_mngrApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct sn_mngrApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(viewMod: LoginViewModel())
         }
     }
 }
