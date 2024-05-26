d62e779b5b1c3336d0d6e8d5a5004c5bbc041671
saddydead <luckybeyhuk@gmail.com>
2024-05-26 22:39:11 +0300

    pizdec

--- python-client/main.py
+++ python-client/main.py
@@ -1,17 +1,6 @@
 import customtkinter as ctk
 import login_screen
 import main_screen
-from threading import Thread
-
-
-class LoginThread(Thread):
-    def __init__(self, name, func):
-        Thread.__init__(self)
-        self.name = name
-        self.func = func
-
-    def run(self):
-        self.func()
 
 
 class App(ctk.CTk):
@@ -19,9 +8,7 @@ class App(ctk.CTk):
         super().__init__()
         self.geometry("300x200")
         self.title("Sonoma Manager v3")
-        LoginThread.run("log in check", self.check_log_in())
 
-    def check_log_in(self):
         if not login_screen.log_in:
             self.login_frame = login_screen.LoginFrame(master=self)
             self.login_frame.pack(anchor="center")
